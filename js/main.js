fe27f1f67d123f0195ae1e55598e9b55aaef771f
AeroztaR <jorguinhos89@gmail.com>
2019-03-15 04:10:03 +0500

    add localStorage

--- js/main.js
+++ js/main.js
@@ -49,9 +49,53 @@ class UI {
     }
 }
 
+// Local Storage Class
+class Store {
+    static getBooks() {
+        let books;
+        if(localStorage.getItem('books') === null) {
+            books = [];
+        } else {
+            books = JSON.parse(localStorage.getItem('books'));
+        }
 
+        return books;
+    }
 
+    static displayBooks() {
+        const books = Store.getBooks();
 
+        books.forEach(function(book) {
+            const ui = new UI;
+
+            // add book to UI
+            ui.addBookToList(book);
+        });
+    }
+
+    static addBook(book) {
+        const books = Store.getBooks();
+
+        books.push(book);
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+
+    static removeBook(isbn) {
+        const books = Store.getBooks();
+
+        books.forEach(function(book, index) {
+            if(book.isbn === isbn) {
+                books.splice(index, 1);
+            }
+        });
+
+        localStorage.setItem('books', JSON.stringify(books));
+    }
+}
+
+// DOM Loaded Event
+document.addEventListener('DOMContentLoaded', Store.displayBooks);
 
 // Event Listener for add book
 document.getElementById('book-form').addEventListener('submit', function (e) {
@@ -76,6 +120,9 @@ document.getElementById('book-form').addEventListener('submit', function (e) {
         // add book to list
         ui.addBookToList(book);
 
+        // add to localStorage
+        Store.addBook(book);
+
         // show success
         ui.showAlert('Book Added!', 'success');
 
@@ -92,6 +139,9 @@ document.getElementById('book-list').addEventListener('click', function(e) {
 
     // delete book
     ui.deleteBook(e.target);
+
+    // remove from localStorage
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
 
     // show message
     ui.showAlert('Book Deleted!', 'success');
